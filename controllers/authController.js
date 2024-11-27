7cd9c296d0937494b373a58f9764cbb816ca3103
Deniz Karakaş <96745481+denizkarakass@users.noreply.github.com>
2024-11-27 20:02:11 +0300

    forgot pass and category error solved

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,17 +1,16 @@
-const bcrypt = require('bcrypt');  //Şifreleri veritabanında şifreli tutuyoruz
-const User = require('../models/User');
-const Category = require('../models/Category');
-const Book = require('../models/Book');
-
+const bcrypt = require("bcrypt"); //Şifreleri veritabanında şifreli tutuyoruz
+const User = require("../models/User");
+const Category = require("../models/Category");
+const Book = require("../models/Book");
 
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
 
-    res.status(201).redirect('/');
+    res.status(201).redirect("/");
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
+      status: "fail",
       error,
     });
   }
@@ -21,43 +20,132 @@ exports.loginUser = (req, res) => {
   try {
     const { email, password } = req.body;
 
-     User.findOne({ email }, (err, user) => {
+    User.findOne({ email }, (err, user) => {
       if (user) {
         bcrypt.compare(password, user.password, (err, same) => {
-            req.session.userID = user._id; //user sessionlar
-            res.status(200).redirect('/users/dashboard');
+          req.session.userID = user._id; //user sessionlar
+          res.status(200).redirect("/users/dashboard");
         });
       }
     });
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
+      status: "fail",
       error,
     });
   }
 };
 
-
 exports.logoutUser = (req, res) => {
-  req.session.destroy(()=> {
-    res.redirect('/');
-  })
-}
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+};
 
-
-exports.getDashboardPage = async(req, res) => {
+exports.getDashboardPage = async (req, res) => {
   /*1)Kullanıcının id bilgilerini yakalayıp veritabanından bilgilerini  alıcam (yönetim paneli kısmında kullanmak için)
-    2) Populate kullanarak kullanıcı ve kitap arasında ilişki kurucam ki user üzerinden kitap bilgilerine ulaşabileyim */ 
-  const user = await User.findOne({_id:req.session.userID}).populate('books');  
+    2) Populate kullanarak kullanıcı ve kitap arasında ilişki kurucam ki user üzerinden kitap bilgilerine ulaşabileyim */
+  const user = await User.findOne({ _id: req.session.userID }).populate(
+    "books"
+  );
   const users = await User.find();
   //console.log(req.session.userID);
   const categories = await Category.find();
-  const books = await Book.find({user:req.session.userID}); /* kullanıcı id'si sessiondan kontrol edilen user ID'ye eşit olan kitapları çekiyorum
+  const books = await Book.find({
+    user: req.session.userID,
+  }); /* kullanıcı id'si sessiondan kontrol edilen user ID'ye eşit olan kitapları çekiyorum
   Bu sayede kullanıcının yayınladığı kitapları onun sayfasında gösterebiliyorum. */
-  res.status(200).render('dashboard', {
-      page_name: "dashboard",
-      categories,
-      user,
-      books
+  res.status(200).render("dashboard", {
+    page_name: "dashboard",
+    categories,
+    user,
+    books,
+  });
+};
+
+function sendEmail(mail, msg, subject) {
+  // Mail gönderme işlemi
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "terapistimburada@gmail.com", // Gönderen e-posta adresi
+      pass: "wakeljfqfnwscdaw", // Gönderen e-posta hesabının şifresi
+    },
+  });
+  const mailOptions = {
+    from: "hr@terapistimburada.com", // Gönderen e-posta adresi
+    to: mail, // Kullanıcının e-posta adresi
+    subject: subject, // E-posta konusu
+    html: `
+    <p>${msg}</p>
+    <br>
+     `, // E-posta içeriği (metin formatı)
+    // html: '<p>Lütfen hesabınızı onaylamak için <a href="http://example.com/onayla">tıklayın</a></p>' // E-posta içeriği (HTML formatı)
+  };
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("E-posta gönderildi: " + info.response);
+    }
   });
 }
+
+exports.forgotPassword = async (req, res) => {
+  //Nodemailer ile mail gönderme işlemi
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(404).json({
+      message: "User not found",
+    });
+  }
+  const token = await Math.random().toString(36).substr(2, 10);
+  await user.save();
+  const resetPasswordUrl = `http://localhost:3000/resetPassword?token=${token}`;
+  const emailTemplate = `
+    <h3>Reset Your Password</h3>
+    <p>This <a href='${resetPasswordUrl}' target='_blank'>link</a> will expire in 1 hour</p>
+  `;
+
+  try {
+    await sendEmail({
+      from: process.env.SMTP_USER,
+      to: email,
+      subject: "Reset Your Password",
+      html: emailTemplate,
+    });
+    return res.status(200).json({
+      message: "Email sent",
+    });
+  } catch (error) {
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    return res.status(500).json({
+      message: "Email could not be sent",
+    });
+  }
+};
+
+exports.resetPassword = async (req, res) => {
+  const { token } = req.query;
+  const resetPasswordToken = crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+
+  const user = await User.findOne({
+    resetPasswordToken,
+    resetPasswordExpire: { $gt: Date.now() },
+  });
+
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid token",
+    });
+  }
+  res.status(200).render("resetPassword", {
+    token,
+  });
+};
--- controllers/bookController.js
+++ controllers/bookController.js
@@ -1,8 +1,7 @@
-const slugify = require('slugify');
-const Book = require('../models/Book');
-const User = require('../models/User');
-const Category = require('../models/Category');
-
+const slugify = require("slugify");
+const Book = require("../models/Book");
+const User = require("../models/User");
+const Category = require("../models/Category");
 
 /*
 Kullanıcının kitap oluşturması için gereken fonksiyon
@@ -10,8 +9,8 @@ Async-await yapısını kullandım
 Hata ile karşılaşırsam yakalamak için try-catch bloğu kullandım
 Front-end tasarımını daha hazırlamamıştım bu yüzden önce json ile yazdırttım çalışmamın kontrolünün sağladım. POSTMAN uygulaması ile istekleri gönderdim.
 */
-exports.createBook = async (req,res) => {
-   try{
+exports.createBook = async (req, res) => {
+  try {
     const book = await Book.create({
       baslik: req.body.baslik,
       desciription: req.body.desciription,
@@ -24,19 +23,19 @@ exports.createBook = async (req,res) => {
       sayfa: req.body.sayfa,
       dili: req.body.dili,
       category: req.body.category,
-      user: req.session.userID  //giriş yapan kullanıcının USER ID 'sini alma session'dan alma
+      user: req.session.userID, //giriş yapan kullanıcının USER ID 'sini alma session'dan alma
     });
-    res.status(201).redirect('/books');/*.json({
+    res.status(201).redirect("/books"); /*.json({
         status: 'success',
         book
     })*/
-   }catch(error){
+  } catch (error) {
     res.status(400).json({
-        status: 'fail in create' ,
-        error
-    }) 
-   }
-}
+      status: "fail in create",
+      error,
+    });
+  }
+};
 
 /*Tüm kitapları bulup listelemek için frontend öncesi
 exports.getAllBooks = async (req,res) => {
@@ -55,133 +54,133 @@ exports.getAllBooks = async (req,res) => {
  }
 */
 
-//Tüm kitapları bulup listelemek için 
-exports.getAllBooks = async (req,res) => {
-    try{
-      const categorySlug = req.query.categories;  //parmetreden gelen sorguyu aldım
-      const query = req.query.search; //Kitap arama sorgusu tanımı
+//Tüm kitapları bulup listelemek için
+exports.getAllBooks = async (req, res) => {
+  try {
+    const categorySlug = req.query.categories; //parmetreden gelen sorguyu aldım
+    const query = req.query.search; //Kitap arama sorgusu tanımı
 
-      const category = await Category.findOne({slug:categorySlug})
-     
-      //Filtreleme işlemi
-      let filter = {};
-      if(categorySlug) {
-        filter = {category:category._id}
-      }
-      
-      //Kitap arama yapma işlemi
-      if(query) {
-        filter = {baslik:query}
-      }
-  
-      if(!query && !categorySlug) {
-        filter.baslik = "",
-        filter.category = null
-      }
-  
-      const books = await Book.find({
-        $or:[
-          {baslik: { $regex: '.*' + filter.baslik + '.*', $options: 'i'}},
-          {category: filter.category}
-        ]
-      }).sort('-createdAt').populate('user');
+    const category = await Category.findOne({ name: categorySlug });
 
-
-
-     const categories = await Category.find();
-
-     res.status(200).render('books',{
-         books,
-         categories,
-         page_name:'books',
-     })
-    }catch(error){
-     res.status(400).json({
-         status: 'fail in getallbooks' ,
-         error
-     }) 
+    //Filtreleme işlemi
+    let filter = {};
+    if (categorySlug) {
+      filter = { category: category._id };
     }
- }
 
- //Kitap sayfalarına gitmek için 
- exports.getBook = async (req, res) => {
-    try {
-      const user = await User.findById(req.session.userID); /*Kullanıcıyı yakaladım. Eğer kullanıcı kitabı ödünç almışsa tekrar
+    //Kitap arama yapma işlemi
+    if (query) {
+      filter = { baslik: query };
+    }
+
+    if (!query && !categorySlug) {
+      (filter.baslik = ""), (filter.category = null);
+    }
+
+    const books = await Book.find({
+      $or: [
+        { baslik: { $regex: ".*" + filter.baslik + ".*", $options: "i" } },
+        { category: filter.category },
+      ],
+    })
+      .sort("-createdAt")
+      .populate("user");
+
+    const categories = await Category.find();
+
+    res.status(200).render("books", {
+      books,
+      categories,
+      page_name: "books",
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail in getallbooks",
+      error,
+    });
+  }
+};
+
+//Kitap sayfalarına gitmek için
+exports.getBook = async (req, res) => {
+  try {
+    const user = await User.findById(
+      req.session.userID
+    ); /*Kullanıcıyı yakaladım. Eğer kullanıcı kitabı ödünç almışsa tekrar
        almaması için if koşulu yapmam gerekecek bunun için user bilgileri gerekiyordu. */
-      const book = await Book.findOne({slug: req.params.slug}).populate('user');/* Populate işlemi yaptım burada kullanıcı bilgilerinide
+    const book = await Book.findOne({ slug: req.params.slug }).populate(
+      "user"
+    ); /* Populate işlemi yaptım burada kullanıcı bilgilerinide
       ulaşabilmek için  kullanma yönntemim şöyle: <%= book.user.baslik %> --> Kitabın kim tarafından oluşturulduğunu yazdırabilirim bu şekilde
       */
 
-      const categories = await Category.find();
-  
-      res.status(200).render('book', {
-        book,
-        page_name: 'books',
-        categories,
-        user
-      });
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        error,
-      });
-    }
-  };
-  
+    const categories = await Category.find();
+
+    res.status(200).render("book", {
+      book,
+      page_name: "books",
+      categories,
+      user,
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
 //Kitap alma işlemi
-  exports.alBook = async (req, res) => {
-    try {
-      const book = await Book.findOne({slug:req.params.slug});
-      const user = await User.findById(req.session.userID);
-      await user.books.push({_id:req.body.book_id});  //Kitapı almak için push methodunu kullandım
-      await user.save();
-  
-      res.status(200).redirect('/users/dashboard');
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail in all book',
-        error,
-      });
-    }
-  };
+exports.alBook = async (req, res) => {
+  try {
+    const book = await Book.findOne({ slug: req.params.slug });
+    const user = await User.findById(req.session.userID);
+    await user.books.push({ _id: req.body.book_id }); //Kitapı almak için push methodunu kullandım
+    await user.save();
 
-  //Kitap iade etme işlemi
-  exports.etBook = async (req, res) => {
-    try {    
-      const user = await User.findById(req.session.userID);
-      await user.books.pull({_id:req.body.book_id});  //Kitapı iade etmek için pull methodunu kullandım
-      await user.save();
-  
-      res.status(200).redirect('/users/dashboard');
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        error,
-      });
-    }
-  };
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail in all book",
+      error,
+    });
+  }
+};
 
-  //Kitap silme işlemi
-  exports.silBook = async (req, res) => {
-    try {    
-  
-      const book = await Book.findOneAndRemove({slug:req.params.slug})  
-    
-      res.status(200).redirect('/users/dashboard');
-  
-    } catch (error) {
-      res.status(400).json({
-        status: 'fail',
-        error,
-      });
-    }
-  };  
+//Kitap iade etme işlemi
+exports.etBook = async (req, res) => {
+  try {
+    const user = await User.findById(req.session.userID);
+    await user.books.pull({ _id: req.body.book_id }); //Kitapı iade etmek için pull methodunu kullandım
+    await user.save();
+
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
+
+//Kitap silme işlemi
+exports.silBook = async (req, res) => {
+  try {
+    const book = await Book.findOneAndRemove({ slug: req.params.slug });
+
+    res.status(200).redirect("/users/dashboard");
+  } catch (error) {
+    res.status(400).json({
+      status: "fail",
+      error,
+    });
+  }
+};
 
 //Kitap güncelleme işlemi
 exports.güncelleBook = async (req, res) => {
-  try {    
-
-    const book = await Book.findOne({slug:req.params.slug});
+  try {
+    const book = await Book.findOne({ slug: req.params.slug });
     book.baslik = req.body.baslik;
     book.desciription = req.body.desciription;
     book.yazar = req.body.yazar;
@@ -194,11 +193,10 @@ exports.güncelleBook = async (req, res) => {
     book.dili = req.body.dili;
     book.save();
 
-    res.status(200).redirect('/users/dashboard');
-
+    res.status(200).redirect("/users/dashboard");
   } catch (error) {
     res.status(400).json({
-      status: 'fail',
+      status: "fail",
       error,
     });
   }
